b092afa7ef13d8e14048849b480f7f009c25a643
ismailsosic <ismailsosic@gmail.com>
2021-10-16 05:31:04 +0200

    add dashboard navbar

---
+++ src/components/dashboard/partials/welcome/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Welcome;
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './Login.css';
-import logo from '../../img/logo.JPG'
 
 const Login = () => {
     return (
         <div className="login">
-            <div className="logo">
-                <img src={logo} alt="logo"/>
-            </div>
             <div className="loginForm">
-                <form action="" method="POST">
+                <form action="/dashboard" method="">
                     <h1>LOGIN</h1>
                     <p>Please enter your username and password!</p>
                     <div className="inputFields">
